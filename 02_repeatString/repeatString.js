a0a9137a34a0227cd17712a381366491ca1e6e6d
Dylan Haines <90574712+dylanjhaines@users.noreply.github.com>
2022-12-30 20:39:06 -0800

    Create 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+//let str = prompt("enter text to be repeated", "hey");
+//let num = prompt("enter number of times to repeat", "10");
+let word;
+let num;
+const repeatString = function(word, num) {
+    let string = '';
+    
+    if (num < 0) return 'ERROR';
+    
+    for (i=0; i < num; i++) {
+      string += word;
+    }
+    
+return string;
 };
 
 // Do not edit below this line
