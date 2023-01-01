327438f8c343eb1cc9293cf8de7bd31d6fc169dd
Dylan Haines <90574712+dylanjhaines@users.noreply.github.com>
2023-01-01 10:52:49 -0800

    create 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+let input1 = [];
+const reverseString = function(input1) {
+//receive input
 
+//create loop and have initiator start at end of input
+//as initiator counts down through input, assign [] as output
+//return output
+//let input1 = "hello"
+let output1 = '';
+for (let i = input1.length - 1; i >= 0; i--){
+    let letter = input1[i];
+    output1 += letter;
+    console.log(output1);
+}
+
+return output1
 };
 
 // Do not edit below this line
