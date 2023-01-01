aa9c3f20fea6bc9b5ece1394c179ae5a2774e392
Dylan Haines <90574712+dylanjhaines@users.noreply.github.com>
2023-01-01 13:15:35 -0800

    create 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  //accept an array as input
+  //begin a loop at the first number of the array [0]
+  //end the loop at the last number of the array [args.length-1]
+  //for each iteration add the index to the return value
+  
+  if ((min || max) < 0) return "ERROR";
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
 
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+return sum;
 };
 
 // Do not edit below this line
