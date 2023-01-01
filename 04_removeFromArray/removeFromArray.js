9e9460bfabfa83cf63d4e27595c2d6cdd22ce1f4
Dylan Haines <90574712+dylanjhaines@users.noreply.github.com>
2023-01-01 12:26:00 -0800

    lost on this one

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(...theArgs) {
+    const array = theArgs[0];
+    let removedArray = [];
+   
+    array.forEach((item) => {
+        if (!theArgs.includes(item)) {
+            removedArray.push(item);
+        }
+    });
+
+
+    return removedArray;
 };
 
 // Do not edit below this line
