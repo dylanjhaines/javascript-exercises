f676053194843729ae2e05271b0f043971520ce6
Dylan Haines <90574712+dylanjhaines@users.noreply.github.com>
2023-01-22 14:05:33 -0800

    create palindrome detector

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+let lowerString = string.toLowerCase();
+let processedString = lowerString.replace(/[^a-zA-Z]/g,"");
+let reversedString = processedString
+    .split("")
+    .reverse()
+    .join("")
+return reversedString == processedString;
 };
 
 // Do not edit below this line
