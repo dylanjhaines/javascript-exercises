af67e532835c676d7b8aac619fa2a4d39fff57a2
Dylan Haines <90574712+dylanjhaines@users.noreply.github.com>
2023-01-22 14:41:43 -0800

    create fib js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let array = [1];
+    array[1] = 1;
+    if (num < 1) {
+        return "OOPS";
+    }
+    for (i = 2; i < num; i++) {
+        array[i] = array[i-1]+array[i-2]
+    }
+    return parseInt(array.slice(-1));
 };
 
 // Do not edit below this line
