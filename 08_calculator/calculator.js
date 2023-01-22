52e5651963d30b8752b94e5bc9820f8abfcf01ad
Dylan Haines <90574712+dylanjhaines@users.noreply.github.com>
2023-01-22 13:16:20 -0800

    create js calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let num2 = 1
+    while (num1 > 0) {
+      num2 *= num1;
+      num1--;
+    }
+  return num2;
 };
 
 // Do not edit below this line
